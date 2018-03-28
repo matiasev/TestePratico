e9da7181f4fdb428a78afca8baa55efd87677d54
Evandro Matias <evandromattias@gmail.com>
2018-03-28 17:29:20 -0300

    Commit Initial

--- TestePratico/Controllers/HomeController.cs
+++ TestePratico/Controllers/HomeController.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using TestePratico.Models;
-using TestePratico.Models.AccountViewModels;
 
 namespace TestePratico.Controllers
 {
